aa083aa1b67451f8dfd3c5694f17b3c50e90da15
Campanita (sana) <rapunzel@disroot.org>
2025-05-21 04:01:47 -0600

    Modelo II, config de config predeterminado

---
+++ doc/config.sql
@@ -0,0 +1,5 @@
+INSERT INTO configuracion (nombre, descripcion, tipo, valor) VALUES 
+('prod_generico', 'Habilitar producto genérico', 0, TRUE),
+('redondeo', 'Habilitar redondeo de centavos', 1, 0),
+('cobrar_menos', 'Permitir cobrar menos del importe total de la venta', 0, FALSE),
+('precio_con_imp', 'Los precios incluyen los impuestos', 0, TRUE);
